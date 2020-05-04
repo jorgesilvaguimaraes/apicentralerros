00c7b2dbc9995c20cde731b5e6b849085e556e31
Jorge da Silva Guimarães <jorgeguimaraes@cinbesa.com.br>
2020-05-04 11:09:12 -0300

    add redirect /swagger-ui.html

--- src/main/java/br/com/apicentralerros/ApicentralerrosApplication.java
+++ src/main/java/br/com/apicentralerros/ApicentralerrosApplication.java
@@ -1,8 +1,17 @@
 package br.com.apicentralerros;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.Collections;
+import java.util.Map;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -10,6 +19,20 @@ public class ApicentralerrosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApicentralerrosApplication.class, args);
+	}
+
+
+	@RestController
+	@RequestMapping(path = "/")
+	class Docs {
+
+		@GetMapping(value = "/")
+		public RedirectView swagger() {
+			RedirectView redirectView = new RedirectView();
+			redirectView.setUrl("https://api-centralerros.herokuapp.com/swagger-ui.html");
+			return redirectView;
+		}
+
 	}
 
 }
